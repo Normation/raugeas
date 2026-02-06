8807c129140a777b1a20e4c83510340cba6b3bec
Alexis Mousset <alexis.mousset@rudder.io>
2026-02-06 22:13:32 +0100

    Update tooling and dependencies
    Update MSRV to 1.89.0
    Update to edition 2024

--- raugeas/examples/userinfo.rs
+++ raugeas/examples/userinfo.rs
@@ -1,19 +1,21 @@
 use raugeas::{Augeas, Flags};
+use std::error::Error;
 
-fn main() {
-    let aug = Augeas::init(Some("tests/test_root"), "", Flags::NONE).unwrap();
+fn main() -> Result<(), Box<dyn Error>> {
+    let aug = Augeas::init(Some("tests/test_root"), "", Flags::NONE)?;
     let username = "root";
 
     println!("Infos about '{}':", username);
 
-    let info_nodes = aug.matches("etc/passwd/root/*").unwrap();
+    let info_nodes = aug.matches("etc/passwd/root/*")?;
 
     for node in info_nodes.iter() {
-        let label = aug.label(node).unwrap();
-        let value = aug.get(node).unwrap();
+        let label = aug.label(node)?;
+        let value = aug.get(node)?;
 
         if let (Some(label), Some(value)) = (label, value) {
             println!("{} = {}", label, value)
         }
     }
+    Ok(())
 }
